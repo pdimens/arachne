7c1c4b9c77585e017c7b4cd07e65a70d6523085d
pdimens <pdimens@gmail.com>
2026-06-03 10:59:40 -0400

    add pixi tests/envs

---
+++ test/data.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python3
+"""Creates a FASTA file with 2 contigs of 200k random nucleotides"""
+import random
+import os
+
+random.seed(6969)
+os.makedirs('test', exist_ok = True)
+
+nuc = "ATCG"
+with open("test/hap1.fa", "w") as fa:
+    for i in [1,2]:
+        _ = fa.write(f">Contig{i}\n" + "".join(random.choices(nuc, k = 200000)) + "\n")
+
+with open("test/hap2.fa", "w") as fa:
+    for i in [1,2]:
+        _ = fa.write(f">Contig{i}\n" + "".join(random.choices(nuc, k = 200000)) + "\n")
