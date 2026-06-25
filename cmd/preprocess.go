aae267a9825a80716d480c0c9454df51747d7211
pdimens <pdimens@gmail.com>
2026-06-25 16:53:40 -0400

    fine-tune the CLI

--- cmd/preprocess.go
+++ cmd/preprocess.go
@@ -3,6 +3,7 @@ package cmd
 
 import (
 	"fmt"
+	"runtime"
 
 	"arachne/preprocess"
 
@@ -12,14 +13,19 @@ import (
 // preprocessCmd represents the preprocess command
 var preprocessCmd = &cobra.Command{
 	Use:     "preprocess [-t] PREFIX R1.fq R2.fq",
-	Short:   "Format FASTQ files for alignment",
+	Short:   "Format FASTQ files for alignment \033[94;1m[start here]\033[0m",
 	Example: "preprocess -t 12 sample1 smp1.R1.fq.gz smp1.R2.fq.gz",
-	Long: "The command converts a set of paired-end FASTQ files into the format required for the arachne aligner. " +
-		"For arachne to work correctly, input FASTQ files need to be in \"standard\" format and sorted by barcode (at minimum). " +
-		"Reads with invalid and singleton barcodes will be preserved separately so they can be aligned using another tool like BWA.",
+	Long: "Converts a set of paired-end FASTQ files into the format required for the arachne aligner. " +
+		"For arachne to work correctly, input FASTQ files need to be properly paired, in \"standard\" format " +
+		"(\033[94;1mBX:Z\033[0m and \033[94;1mVX:i\033[0m tags), and sorted by barcode. " +
+		"Reads with invalid barcodes will be preserved separately so they can be aligned using another tool like BWA.",
 	DisableFlagsInUseLine: true,
 	SilenceUsage:          true,
 	Args: func(cmd *cobra.Command, args []string) error {
+		if len(args) == 0 {
+			fmt.Printf("%s", cmd.UsageString())
+			return fmt.Errorf("please provide inputs")
+		}
 		if err := cobra.ExactArgs(3)(cmd, args); err != nil {
 			return err
 		}
@@ -38,7 +44,10 @@ var preprocessCmd = &cobra.Command{
 		if err != nil {
 			return err
 		}
-		threads = max(threads, 1)
+		maxCores := runtime.NumCPU()
+		// clamp between 1 and max system threads
+		threads = min(maxCores, max(threads, 1))
+		runtime.GOMAXPROCS(threads)
 		preprocess.Preprocess(threads, args[0], args[1], args[2])
 		return nil
 	},
@@ -46,5 +55,5 @@ var preprocessCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(preprocessCmd)
-	preprocessCmd.Flags().IntP("threads", "t", 2, fmt.Sprintf("Number of threads to use (min: %d)", 2))
+	preprocessCmd.Flags().IntP("threads", "t", 2, "Number of threads to use")
 }
--- cmd/root.go
+++ cmd/root.go
@@ -14,15 +14,7 @@ var rootCmd = &cobra.Command{
 	Version: aligner.VERSION,
 	Short:   "A linked-read sequence aligner",
 	Long: "Arachne linked-read aligner\n\nArachne is a successor to 10X Genomics' Lariat aligner that " +
-		"is compatible with all current linked-read technologies (e.g. haplotagging, stLFR, TELLseq). " +
-		"Input FASTQ files can come from any linked-read technology, provided they:\n" +
-		"  - are a set of paired-end reads\n" +
-		"  - are in \"standard\" format\n" +
-		"    - have barcodes in a \033[92;1mBX:Z\033[0m SAM tag\n" +
-		"      - e.g. \033[92;1mBX:Z:ATGGACTAGA\033[0m\n" +
-		"    - have barcode validations (0|1) in a \033[92;1mVX:i\033[0m SAM tag\n" +
-		"      - \033[92;1mVX:i:1\033[0m if valid, \033[92;1mVX:i:0\033[0m if invalid\n" +
-		"  - are sorted by barcode\n\n",
+		"is compatible with all current linked-read technologies (e.g. haplotagging, stLFR, TELLseq), provided they are preprocessed correctly.",
 }
 
 func init() {
